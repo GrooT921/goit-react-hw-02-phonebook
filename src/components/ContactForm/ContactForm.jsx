0ea889866b2470a793929f081d6a39e081b60bc1
GrooT921 <rarapin96@gmail.com>
2022-10-15 14:57:07 +0200

    add conract

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,27 +1,44 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
+const phoneRegExp =
+  /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/;
+
 const userSchema = Yup.object({
   name: Yup.string().required(),
+  number: Yup.string()
+    .matches(phoneRegExp, 'Phone number is not valid')
+    .required(),
 });
 
-export const ContactForm = ({ contacts, name }) => {
-  const handleSubmit = (values, actions) => {
-    console.log(values);
+export const ContactForm = ({ addContact }) => {
+  const handleSubmit = ({ name, number }, actions) => {
+    addContact(name, number);
+    console.log(name, number);
+    actions.resetForm();
   };
   return (
     <Formik
-      initialValues={{ name: '' }}
+      initialValues={{ name: '', number: '' }}
       onSubmit={handleSubmit}
-      validationSchems={userSchema}
+      validationSchema={userSchema}
     >
       <Form>
         <label>
-          <span>Name</span>
+          Name
           <Field
             type="text"
             name="name"
+            // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            // title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          />
+        </label>
+        <label>
+          Number
+          <Field
+            type="text"
+            name="number"
             // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             // title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           />
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,1 +1,16 @@
-export const ContactList = () => {};
+export const ContactList = ({ contacts }) => {
+  return (
+    <div>
+      <ul>
+        {contacts.map(item => {
+          return (
+            <li key={item.id}>
+              <span>{item.name}</span>
+              <span>{item.number}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
